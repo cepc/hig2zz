57871ce80a9133e547351fc7e4996223c28a48c0
Ryuta Kiuchi <kiuchi@ihep.ac.cn>
2021-03-10 13:23:12 -1000

    adjust scripts to the central environment

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ cd ./MarlinFastJet-00-02
 rm -fr build
 mkdir build
 cd build
-#cmake -C ../ILCSoft.cmake ..
-cmake -C ../ILCSoft.cmake.mod ..
+cmake ../
 make install 
 cd ../../
 
@@ -17,8 +16,7 @@ cd ../../
 rm -fr build
 mkdir build
 cd build
-#cmake -C /afs/ihep.ac.cn/soft/common/gcc/v01-17-05/ILCSoft.cmake .. 
-cmake -C /cefs/higgs/kiuchi/Higgs2zz/Emergency_Copied_Marlin/ILCSoft.cmake ..
+cmake ../
 make install
 cd ..
 
--- python/get_bg_samples.py
+++ python/get_bg_samples.py
@@ -38,13 +38,14 @@ def main():
             file_list = []
             for root, dirs, files in os.walk(src):
                 for f in files:
-                    if f.startswith(name_tag):
-                        if f.endswith('.slcio'):
-                            file_time = os.path.getmtime(os.path.join(root, f)) 
-                            diff_day = (ctime - file_time)/3600.0/24.0  
-                            #if diff_day > 1.0 : ## to avoid access to generating slcio files
-                            if diff_day > 0.0 :
-                                file_list.append(os.path.join(root, f))                                
+                    if os.path.exists(os.path.join(root, f)):
+                        if f.startswith(name_tag):
+                            if f.endswith('.slcio'):
+                                file_time = os.path.getmtime(os.path.join(root, f)) 
+                                diff_day = (ctime - file_time)/3600.0/24.0  
+                                #if diff_day > 1.0 : ## to avoid access to generating slcio files
+                                if diff_day > 0.0 :
+                                    file_list.append(os.path.join(root, f))                                
 
             fname = l[0]   ## filename = dirname for the moment !            
             if len(args) == 3:
